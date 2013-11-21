b97302043c276efa74518c30b1e5105cc18509de
Jonty Wareing <jonty@jonty.co.uk>
2013-11-21 17:14:31 +0000

    Add ALERT/FIXED, add colours

--- sd-irccat.py
+++ sd-irccat.py
@@ -17,6 +17,7 @@ account = config.get('serverdensity', 'account')
 auth = (config.get('serverdensity', 'username'), config.get('serverdensity', 'password'))
 
 alerts_seen = set()
+first_run = True
 
 while True:
     response = requests.get('https://api.serverdensity.com/1.4/alerts/getLast?account=%s' % account, auth=auth)
@@ -25,25 +26,36 @@ while True:
         service = alert['device']['name']
         alert = alert['alert']
 
-        alert_uniq = alert['alertId'] + alert['timeAlertedGMT']
+        alert_uniq = alert['alertId'] + alert['timeAlertedGMT'] + alert['fixed']
         url = "https://%s/alerts/history/%s" % (account, alert['alertId'])
 
-        if alert['fixed'] == '1' or alert_uniq in alerts_seen:
+        if alert_uniq in alerts_seen:
             continue
 
-        alert_string = "[%s] %s (Value is %s) - %s" % (service, alert['checkTypeText'], alert['triggeredValue'], url)
+        if alert['fixed'] == '0':
+            alert_type = 'ALERT'
+            alert_colour = 'RED'
+        else:
+            alert_type = 'FIXED'
+            alert_colour = 'GREEN'
+
+        alert_string = "[#BOLD#%s%s#NORMAL %s] %s (Value is %s) - %s" % \
+            (alert_colour, alert_type, service, alert['checkTypeText'], alert['triggeredValue'], url)
         log.info(alert_string)
 
-        for channel in config.get('irccat', 'channels').split(','):
-            channel = channel.strip()
+        if first_run == False:
 
-            log.info('Sending alert to %s', channel)
+            for channel in config.get('irccat', 'channels').split(','):
+                channel = channel.strip()
 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((config.get('irccat', 'host'), int(config.get('irccat', 'port'))))
-            s.sendall("%s %s" % (channel, alert_string))
-            s.close()
+                log.info('Sending alert to %s', channel)
+
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((config.get('irccat', 'host'), int(config.get('irccat', 'port'))))
+                s.sendall("%s %s" % (channel, alert_string))
+                s.close()
 
         alerts_seen.add(alert_uniq)
 
+    first_run = False
     time.sleep(10)
